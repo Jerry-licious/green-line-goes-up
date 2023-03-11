097ceecf108e5b0e6ac03c4b81aacf3a286b691d
Jerry <47512608+Jerry-Licious@users.noreply.github.com>
2023-03-10 21:49:35 -0500

    Failed to add the specialisation system.

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,45 +1,12 @@
 import {Actor} from './simulation/actor';
 import {Good} from './simulation/good';
+import {Simulation} from './simulation/simulation';
 
-console.log(Good.values.length);
+let simulation = new Simulation();
+console.log(simulation);
 
-let sellers: Actor[] = [];
-let buyers: Actor[] = [];
-
-let beliefVolatility: number = 0.6
-
-for (let i = 0; i < 10; i++) {
-    sellers.push(new Actor());
-    buyers.push(new Actor());
+for (let i = 0; i < 3000; i++) {
+    simulation.tick();
+    console.log(Array.from(simulation.markets.values()).map((market) => market.currentExchangePrice).join(", "));
 }
-
-
-function tick() {
-    // Sort the buyers and sellers to allow the maximum number of transactions.
-    // Ascending for sellers.
-    sellers.sort((a, b) => b.expectedMarketPrice - a.expectedMarketPrice);
-    // Descending for buyers.
-    buyers.sort((a, b) => a.expectedMarketPrice - b.expectedMarketPrice);
-    for (let i = 0; i < sellers.length; i++) {
-        let buyer = buyers[i];
-        let seller = sellers[i];
-        let beliefChange = Math.abs(buyer.expectedMarketPrice - seller.expectedMarketPrice) * beliefVolatility;
-        // If the buyer offers a higher price than the seller.
-        if (buyer.expectedMarketPrice >= seller.expectedMarketPrice) {
-            // Transaction succeeds.
-            // Buyer considers paying less next time, seller considers charging more next time.
-            buyer.expectedMarketPrice -= beliefChange;
-            seller.expectedMarketPrice += beliefChange;
-        } else {
-            buyer.expectedMarketPrice += beliefChange;
-            seller.expectedMarketPrice -= beliefChange;
-        }
-    }
-    console.log("Round complete:");
-    console.log(buyers.map((buyer) => buyer.expectedMarketPrice));
-    console.log(sellers.map((seller) => seller.expectedMarketPrice));
-}
-
-for (let i = 0; i < 10; i++) {
-    tick();
-}
+console.log(simulation);
